da006bdc6aa36fefdf30c3e823ce711ffe6c39a7
SivanandAchanta <sivanand.a@research.iiit.ac.in>
2016-10-20 11:12:51 +0530

    added minibatch version

---
+++ dnn_minibatchver/update_params.m
@@ -0,0 +1,22 @@
+
+switch sgd_type
+    
+    case 'sgdcm'
+        
+        [GW, GpdW] = sgdcm(lr, mf, gW, GpdW, GW);
+        [Gb, Gpdb] = sgdcm(lr, mf, gb, Gpdb, Gb);
+        
+    case 'adadelta'
+        
+        [GW, GpmsgW, GpmsxW, GpdW] = adadelta(rho, eps_hp, mf, gW, GpmsgW, GpmsxW, GpdW, GW);
+        [Gb, Gpmsgb, Gpmsxb, Gpdb] = adadelta(rho, eps_hp, mf, gb, Gpmsgb, Gpmsxb, Gpdb, Gb);
+        
+    case 'adam'
+        
+        [GW,GpmW,GpvW]      = adam(alpha,beta1,beta2,eps_hp,lam,num_up,gW,GpmW,GpvW,GW);
+        [Gb,Gpmb,Gpvb]      = adam(alpha,beta1,beta2,eps_hp,lam,num_up,gb,Gpmb,Gpvb,Gb);
+        
+end
+
+
+
